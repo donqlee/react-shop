c93f8ba247b685866b2cb82e39e6931138b771f9
donqlee <donqlee88@gmail.com>
2022-05-05 12:49:12 +0900

    syled-component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,5 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+// props로 컴포넌트 재활용가능
+let Btn = styled.button`
+    background : ${props => props.bg}; 
+    color : ${props => props.bg === 'blue' ? 'white' : 'black'}; 
+    padding : 10px;
+`
+//상속 가능 
+let NewBtn = styled(Btn)`
+    margin : 20px;
+`
+
+
+// styled-components 장점
+// 1. css파일 안열어도 된다.
+// 2. 스타일이 다른 js파일로 오염되지 않음 (오염방지하려면 컴포넌트.module.css도 가능)
+// 3. 페이지 로딩시간 단축 (<style></style>로 넣어줌)
 
 const Detail = (props) => {
     let {id} = useParams(); 
@@ -8,6 +26,10 @@ const Detail = (props) => {
     // 정렬시 id로 id를 찾기 위해서  
     return (
         <div className="container">
+            {/* <Btn bg="blue">버튼</Btn>
+            <Btn bg="orange">버튼</Btn>
+            <NewBtn bg='red'>뉴버튼</NewBtn> */}
+            
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
