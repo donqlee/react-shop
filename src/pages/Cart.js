78a734c6e31c419c4eefba45961380122f0f6f09
donqlee <donqlee88@gmail.com>
2022-05-10 23:41:50 +0900

    redux2

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Table} from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Cart = () => {
+    let a = useSelector((state) => {return state});
+    console.log(a.item);
+
     return (
         <div>
             <Table>
@@ -14,12 +18,20 @@ const Cart = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                        <td>안녕</td>
-                    </tr>
+                    {
+                        a.item.map((v, i) => {
+                            return(
+                                <tr>
+                                    <td>{i + 1}</td>
+                                    <td>{v.name}</td>
+                                    <td>{v.count}</td>
+                                    <td>안녕</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    
+                   
                 </tbody>
             </Table>
         </div>
